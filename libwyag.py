13f9a886935b1eb6018d71e51923c6b858085f99
GeorgeU029 <georgeulloa.fl@gmail.com>
2025-02-28 22:22:46 -0500

    added a the commit tree and the ability to parse through it

--- libwyag.py
+++ libwyag.py
@@ -371,4 +371,69 @@ def log_graphviz(repo,sha,seen):
         p = p.decode("ascii")
         print(f" c_{sha} -> c_{p};")
         log_graphviz(repo, p,seen)        
-       
+
+class GitTreeLeaf(object):   
+    def __init__(self,mode,path,sha):
+        self.mode = mode
+        self.path = path
+        self.sha = sha    
+
+def tree_parse_one(raw,start = 0):
+    #Find the space terminator of the mode
+    x = raw.find(b' ',start)
+    assert x-start == 5 or x-start == 6
+
+    #read the mode
+    mode = raw[start:x]
+    if len(mode) == 5:
+        #Normalize to six bytes.
+        mode = b"0" + mode
+
+    #Find the nULL terminator of the path
+    y = raw.find(b'\x00',x)
+    # and read the path
+    path = raw[x + 1:y]    
+
+    #read the SHA
+    raw_sha =  int.from_bytes(raw [y+2:y+21], "big")
+    #convert it into a hex string, padded to 40 chars
+    #with zeros if needed.
+    sha = format(raw_sha,"040x")
+    return y+21,GitTreeLeaf(mode,path.decode("utf8"),sha)
+
+def tree_parse(raw):
+    pos =0
+    max = len(raw)
+    ret = list()
+    
+    while pos < max:
+        pos, data = tree_parse_one(raw,pos)
+        ret.append(data)
+    return ret
+
+def tree_leaf_sort_key(leaf):        
+    if leaf.mode.startswith(b"10"):
+        return leaf.path
+    else:
+        return leaf.path + "/"
+def tree_serialize(obj):
+    obj.items.sort(key=tree_leaf_sort_key)  
+    ret = b''
+    for i in obj.items:
+        ret += i.mode
+        ret += b' '
+        ret += i.path.encode("utf8")
+        ret += b'\x00'
+        sha = int(i.sha,16)
+        ret += sha.to_bytes(20,byteorder="big")
+    return ret                              
+
+class GitTree(GitObject):
+    fmt = b'tree'
+
+    def deserialize(self,data):
+        self.items = tree_parse(data)
+    def serialize(self):
+        return tree_serialize(self)
+    def init(self):
+        self.items = list()    
